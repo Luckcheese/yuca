6dc276e0f12fb9b8b1acd4ab8f58a7c1f347e463
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2019-12-12 10:35:36 -0300

    Make SearchActivity search products (still not showing results)

--- app/src/main/java/com/luckcheese/yuca/activity/SearchActivity.java
+++ app/src/main/java/com/luckcheese/yuca/activity/SearchActivity.java
@@ -1,13 +1,22 @@
 package com.luckcheese.yuca.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.luckcheese.yuca.R;
+import com.luckcheese.yuca.controller.SearchController;
+import com.luckcheese.yuca.controller.callback.ProductsCallback;
+import com.luckcheese.yuca.model.Product;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity implements ProductsCallback {
+
+    private SearchController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,24 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        controller = new SearchController(this);
+
+        search();
     }
 
+    private void search() {
+        controller.getProducts(this);
+    }
+
+    @Override
+    public void onProductsReceived(List<Product> products) {
+
+    }
+
+    @Override
+    public void onServerError(int statusCode, String message) {
+        View rootView = findViewById(android.R.id.content);
+        Snackbar.make(rootView, message, 5000).show();
+    }
 }
