efd04230786265f1227cc9cb777c137020c86486
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2019-12-12 00:32:08 -0300

    Remove fab button as it won't be used

--- app/src/main/java/com/luckcheese/yuca/activity/SearchActivity.java
+++ app/src/main/java/com/luckcheese/yuca/activity/SearchActivity.java
@@ -2,13 +2,8 @@ package com.luckcheese.yuca.activity;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import com.luckcheese.yuca.R;
 
@@ -20,15 +15,6 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
